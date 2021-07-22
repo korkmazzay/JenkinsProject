abea7d8b81365faf9080b6f527955d80ef56fa88
korkmaz <korkmazzay@gmail.com>
2021-07-22 13:01:08 -0700

    first push

---
+++ src/com/collection/Main.java
@@ -0,0 +1,12 @@
+package com.collection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello - Merhaba");
+            Thread.sleep(1000);
+
+        }
+    }
+}
